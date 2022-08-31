4c3cda1adede7decbb16f4575ca088a0244fa6b0
Magdalena Luz <luzm@ethz.ch>
2022-08-31 16:33:59 +0200

    sample code using forpy https://github.com/ylikx/forpy

---
+++ src/sample_mod/my_functions.py
@@ -0,0 +1,20 @@
+def sumup(limit: int) -> int:
+    res = sum(range(limit + 1))
+    print(f'python-print: summing up to {limit} = {res}')
+    return res
+
+
+def hello_world():
+    print("python-print: hello world")
+
+
+def say_hello(name: str):
+    print(f'hello {name}')
+
+
+def print_args(*args, **kwargs) -> str:
+    print("python-print")
+    print("   ---> Arguments: ", args)
+    print("   ---> Keyword arguments: ", kwargs)
+    return "done sample_functions.print_args"
+
