3245272ea5d647c8b11f9c875c9cd32adea044db
Magdalena Luz <luzm@ethz.ch>
2022-10-05 09:24:07 +0200

    determine comm size

--- src/parallel/fortran/main.f90
+++ src/parallel/fortran/main.f90
@@ -6,7 +6,7 @@ program run_parallel
 
 
     integer ierr, my_rank, left_neighbor
-    integer comm_world, group_world, comm_workers, group_workers
+    integer comm_world, group_world, comm_workers, group_workers, num_procs
     integer(c_int) vdim, GLOBAL_V_NUM, GLOBAL_VX_SIZE
     real(16), DIMENSION(24) :: data, res
 
@@ -22,12 +22,14 @@ program run_parallel
     !call MPI_Group_excl(group_world, 1, 0, group_worker, ierr)  ! process 0 not member
      call MPI_Comm_group(comm_world, group_workers, ierr)
      call MPI_Comm_create(comm_world, group_workers, comm_workers, ierr)
+    call MPI_Comm_size(comm_workers, num_procs, ierr)
 
 
 
-    call mpi_group_rank(comm_workers, my_rank, ierr)
+    call mpi_comm_rank(comm_workers, my_rank, ierr)
+    !call mpi_send
+    print*, my_rank,"out of " , num_procs,  ": hello_world"
 
-    print*, "hello_world"
 
     call mpi_finalize(ierr)
     end program run_parallel
