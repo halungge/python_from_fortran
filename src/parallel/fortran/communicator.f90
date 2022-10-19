2241905f894fcab390e4c25825e14947c86c6fe8
Magdalena Luz <luzm@ethz.ch>
2022-10-19 11:26:17 +0200

    output group and communicator id

--- src/parallel/fortran/communicator.f90
+++ src/parallel/fortran/communicator.f90
@@ -27,6 +27,7 @@ contains
         !call MPI_Group_excl(group_world, 1, 0, group_worker, ierr)  ! process 0 not member
         call MPI_Comm_group(comm_world, group_workers, ierr)
         call MPI_Comm_create(comm_world, group_workers, comm_workers, ierr)
+
         call MPI_Comm_size(comm_workers, num_procs, ierr)
         call MPI_comm_rank(comm_workers, my_rank, ierr)
         left_neighbor = mod((my_rank - 1 + num_procs), num_procs)
@@ -36,7 +37,7 @@ contains
             print *, "communicator.f90: communication pattern:"
         end if
         call mpi_barrier(MPI_COMM_WORLD, ierr)
-        print *, "me ", my_rank, ": left ", left_neighbor, ", right ", right_neighbor
+        print *, "comm:", comm_workers, "me ", my_rank, ": left ", left_neighbor, ", right ", right_neighbor
         call mpi_barrier(MPI_COMM_WORLD, ierr)
     end subroutine setup_comm
 
