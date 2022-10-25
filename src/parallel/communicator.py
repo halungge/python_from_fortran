813f6ff649ebfe116dfc3d2c9d32b1afecb95a76
Magdalena Luz <luzm@ethz.ch>
2022-10-25 10:09:15 +0200

    - convert commmunicator.py to CartComm - add call to py2f for communicator to retrieve fortran number

--- src/parallel/communicator.py
+++ src/parallel/communicator.py
@@ -3,15 +3,19 @@ from mpi4py.MPI import Comm
 
 
 class RingComm:
-    def __init__(self, communicator: Comm, local_grid_size=24):
-        self.ring_comm = MPI.Comm(communicator)
+    def __init__(self, communicator: Comm):
+        self.ring_comm = MPI.Cartcomm(communicator)
+        self.ring_comm.Set_name("python_ringcom")
         self.num_procs = self.ring_comm.Get_size()
         self.my_rank = self.ring_comm.Get_rank()
-        self.left_neighbor_rank = (self.my_rank + 1) % self.num_procs
-        self.right_neighbor_rank = (self.my_rank - 1) % self.num_procs
+        self.left_neighbor_rank, self.right_neighbor_rank = self.ring_comm.Shift(0, 1)
 
     def get_left_rank(self):
         return self.left_neighbor_rank
+
+
+    def get_fortran_comm_id(self):
+        return self.ring_comm.f2py()
 
     def get_right_rank(self):
         return self.right_neighbor_rank
@@ -23,5 +27,11 @@ class RingComm:
         return self.num_procs
 
     def exchangeLeft(self, send_buf, recv_buf):
-        self.ring_comm.Send(send_buf, dest=self.left_neighbor_rank, tag=12)
-        self.ring_comm.Recv(recv_buf, source=self.right_neighbor_rank, tag=12)
+        status = MPI.Status()
+        self.ring_comm.Sendrecv(send_buf, dest=self.left_neighbor_rank, sendtag=12, recvbuf=recv_buf,
+                                source=self.right_neighbor_rank, recvtag=12, status=status)
+
+    def exchangeRight(self, send_buf, recv_buf):
+        status = MPI.Status()
+        self.ring_comm.Sendrecv(send_buf, dest=self.right_neighbor_rank, sendtag=12, recvbuf=recv_buf,
+                                source=self.left_neighbor_rank, recvtag=12, status=status)
